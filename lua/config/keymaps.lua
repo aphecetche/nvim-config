3f3306ffb1b331cb15f34166a466a818da775d68
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2023-06-24 15:43:09 +0200

    add format on save

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,11 @@
-vim.api.nvim_set_keymap("n", "<leader>f", ":lua vim.lsp.buf.format()<CR>", {})
-vim.api.nvim_set_keymap("n", "<leader>F", ":lua vim.lsp.buf.range_formatting()<CR>", {})
+local api = vim.api
+
+api.nvim_set_keymap("n", "<leader>f", ":lua vim.lsp.buf.format()<CR>", {})
+api.nvim_set_keymap("n", "<leader>F", ":lua vim.lsp.buf.range_formatting()<CR>", {})
+
+-- format on write
+local FormatOnWriteGroup = api.nvim_create_augroup("FormatOnWriteGroup", { clear = true })
+api.nvim_create_autocmd("BufWritePre", {
+  command = "silent! lua vim.lsp.buf.format()",
+  group = FormatOnWriteGroup
+})
