34a4ef247ebb9bc03e50a00b2d8aa93bdd0c0210
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2023-06-26 18:17:08 +0200

    wip

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -54,20 +54,20 @@ vim.api.nvim_create_autocmd("LspAttach", {
                 end
                 if client.server_capabilities.renameProvider then
                         -- Renames all references to the symbol under the cursor
-                        bufmap("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>")
+                        bufmap("n", "gR", "<cmd>lua vim.lsp.buf.rename()<cr>")
                 end
                 if client.server_capabilities.codeActionProvider then
                         -- Selects a code action available at the current cursor position
-                        bufmap("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>")
-                        bufmap("x", "<F4>", "<cmd>lua vim.lsp.buf.range_code_action()<cr>")
+                        bufmap("n", "ga", "<cmd>lua vim.lsp.buf.code_action()<cr>")
+                        bufmap("x", "ga", "<cmd>lua vim.lsp.buf.range_code_action()<cr>")
                 end
-                if client.server_capabilities.diagnosticProvider then
-                        -- Show diagnostics in a floating window
-                        bufmap("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>")
-                        -- Move to the previous diagnostic
-                        bufmap("n", ">d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
-                        -- Move to the next diagnostic
-                        bufmap("n", "<d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
-                end
+                -- if client.server_capabilities.diagnosticProvider then -- this is for diag pull model only
+                -- Show diagnostics in a floating window
+                bufmap("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>")
+                -- Move to the previous diagnostic
+                bufmap("n", ">d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
+                -- Move to the next diagnostic
+                bufmap("n", "<d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
+                -- end
         end,
 })
