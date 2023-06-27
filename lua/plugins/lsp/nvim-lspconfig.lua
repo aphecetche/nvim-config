606f4e96cc9373323bbe117436804f822d1a139f
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2023-06-27 11:32:31 +0200

    julia setup

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -14,7 +14,14 @@ local function setup_julials(lspconfig)
         if not lspconfig.julials then
                 return
         end
-        lspconfig.julials.setup({})
+        lspconfig.julials.setup({
+                on_new_config = function(new_config, _)
+                        local julia = vim.fn.expand("~/.julia/environments/nvim-lspconfig/bin/julia")
+                        if require("lspconfig").util.path.is_file(julia) then
+                                new_config.cmd[1] = julia
+                        end
+                end,
+        })
 end
 
 local function setup_lua_ls(lspconfig)
