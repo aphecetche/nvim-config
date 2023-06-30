1abc8eb92c834f96abf79433bf8e88b6ba2c7dee
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2023-06-30 10:51:45 +0200

    played with fold marks on the left side, but find them distracting

--- lua/plugins/folding.lua
+++ lua/plugins/folding.lua
@@ -27,15 +27,53 @@ local function foldTextFormatter(virtText, lnum, endLnum, width, truncate)
         return newVirtText
 end
 
+local function setup(opts)
+        -- vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+        -- vim.o.foldcolumn = '1' -- use '1' to show foldlevel on left of line numbers
+        vim.o.foldcolumn = '0' -- use '1' to show foldlevel on left of line numbers
+        vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
+        vim.o.foldlevelstart = 99
+        vim.o.foldenable = true
+
+        require("ufo").setup(opts)
+
+        -- Using ufo provider need remap `zR` and `zM`
+        vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+        vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+        -- set foldlevel via z{n}
+        for _, lvl in pairs { 1, 2, 3, 4, 5, 6, 7, 8, 9 } do
+                local desc = lvl < 4 and "󰘖 Set Fold Level" or "which_key_ignore"
+                vim.keymap.set(
+                        "n",
+                        "z" .. tostring(lvl),
+                        function() require("ufo").closeFoldsWith(lvl - 1) end,
+                        { desc = desc }
+                )
+        end
+end
+
 return {
-        -- {
-        --         "jghauser/fold-cycle.nvim",
-        --         lazy = true, -- loaded by keymap
-        --         opts = true,
-        -- },
         {
                 "kevinhwang91/nvim-ufo",
-                dependencies = "kevinhwang91/promise-async",
+                dependencies = {
+                        { "kevinhwang91/promise-async" },
+                        -- {
+                        --         "luukvbaal/statuscol.nvim",
+                        --         config = function()
+                        --                 local builtin = require("statuscol.builtin")
+                        --                 require("statuscol").setup({
+                        --                         -- foldfunc = "builtin",
+                        --                         -- setopt = true,
+                        --                         relculright = true,
+                        --                         segments = {
+                        --                                 { text = { builtin.foldfunc },      click = "v:lua.ScFa" },
+                        --                                 { text = { "%s" },                  click = "v:lua.ScSa" },
+                        --                                 { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+                        --                         },
+                        --                 })
+                        --         end,
+                        -- }
+                },
                 event = "BufReadPost",
                 opts = {
                         provider_selector = function(_, ft, _)
@@ -49,8 +87,11 @@ return {
                         -- open opening the buffer, close these fold kinds
                         -- use `:UfoInspect` to get available fold kinds from the LSP
                         close_fold_kinds = { "imports" },
-                        open_fold_hl_timeout = 500,
+                        open_fold_hl_timeout = 200,
                         fold_virt_text_handler = foldTextFormatter,
                 },
+                config = function(_, opts)
+                        setup(opts)
+                end
         },
 }
