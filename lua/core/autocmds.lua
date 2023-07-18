9379dd9a4d00b81a1db352a0d5c5dc9f11c0aa32
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2023-07-18 14:02:38 +0200

    add zen-mode and twilight plugins

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,3 +1,42 @@
+-- As an example, we will create the following mappings:
+--  * <leader>ff find files
+--  * <leader>fr show recent files
+--  * <leader>fb Foobar
+-- we'll document:
+--  * <leader>fn new file
+--  * <leader>fe edit file
+-- and hide <leader>1
+
+-- wk.register({
+--   f = {
+--     name = "file", -- optional group name
+--     f = { "<cmd>Telescope find_files<cr>", "Find File" }, -- create a binding with label
+--     r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File", noremap=false, buffer = 123 }, -- additional options for creating the keymap
+--     n = { "New File" }, -- just a label. don't create any mapping
+--     e = "Edit File", -- same as above
+--     ["1"] = "which_key_ignore",  -- special label to hide it in the popup
+--     b = { function() print("bar") end, "Foobar" } -- you can also pass functions!
+--   },
+-- }, { prefix = "<leader>" })
+
+vim.api.nvim_create_autocmd("FileType", {
+    desc = "Markdown keymaps",
+    callback = function(args)
+        if args.match == "markdown" then
+            local wk = require("which-key")
+            wk.register({
+                m = {
+                    name = "Markdown",
+                    v = { "<cmd>!open -a Marked\\ 2.app '%:p'<cr>", "Open in Marked 2" }
+                },
+
+            }, { prefix = "<leader>" })
+        end
+    end
+})
+
+--:nnoremap <leader>m :silent !open -a Marked\ 2.app '%:p'<cr>
+
 -- LSP keymapping
 
 vim.api.nvim_create_autocmd("LspAttach", {
