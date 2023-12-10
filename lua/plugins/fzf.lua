2097ea43845fed6599b6343fd90503d30910e1b9
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2023-12-10 13:09:13 +0100

    fzf-lua: use fzf-vim profile

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -15,13 +15,14 @@ local M = {
     },
     opts = opts,
     keys = { { "<c-p>", function() require("fzf-lua").files(opts) end, } },
-    cmd = { "Rg", "FzfLua" }
+    cmd = { "Rg", "FzfLua" },
 }
 
 M.config = function()
-    vim.api.nvim_create_user_command("Rg", function(opts)
-        require("fzf-lua").grep({ search = table.concat(opts.fargs, " ") })
-    end, { nargs = "*" })
+    -- vim.api.nvim_create_user_command("Rg", function(opts)
+    --     require("fzf-lua").grep({ search = table.concat(opts.fargs, " ") })
+    -- end, { nargs = "*" })
+    require('fzf-lua').setup({ 'fzf-vim' })
     local wk = require("which-key")
     local fzf = require("fzf-lua")
     wk.register({
